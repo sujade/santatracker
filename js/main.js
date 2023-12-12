0d69fd2c6d11ab7cd5e79e36f2756ecf92ead5ac
sujade <dnmzsu@gmail.com>
2023-12-12 21:11:51 +0100

    Map API added and changes were made.

--- js/main.js
+++ js/main.js
@@ -22,10 +22,63 @@ let countDown = setInterval(function () {
     }
 }, 1000);
 
-const cities = ['New York', 'Tokyo', 'Istanbul', 'Kyoto', 'Bahrain', 'Belgrade', 'Amsterdam', 'Cologne', 'Rome', 'Lisbon', 'Paris', 'London', 'Athens', 'Vienna', 'Delhi', 'Chicago', 'Sydney', 'Kiev', 'Ankara', 'Hamburg', 'Florence']
-const activities = ['is not leaving a single crumb back.', '... wait- is that milk spoiled?', 'is dancing to the beat.', 'trying hard to fit the presents into the socks.', 'is stuck in a chimney.', 'is checking out the naughty list.', 'little Timmy is not on the list.', 'is completing another Python tutorial while flying.', 'is around the world, around the world.', 'this is his last stop for tonight!', 'is full of cookies and milk.', 'is growing an appreciation for K-Pop.']
+class City {
+    constructor(name, latitude, longitude) {
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    }
+}
+const cities = [
+    new City ('New York', 40.730610, -73.935242),
+    new City ('Tokyo', 35.652832, 139.839478),
+    new City ('Istanbul', 41.015137, 28.979530),
+    new City ('Kyoto', 35.011665, 135.768326),
+    new City ('Bahrain',25.9434256, 50.6014985),
+    new City ('Belgrade', 44.787197, 20.457273),
+    new City ('Amsterdam', 52.377956, 4.897070),
+    new City ('Cologne', 50.935173, 6.953101),
+    new City ('Rome', 41.902782, 12.496366),
+    new City ('Lisbon', 38.736946, -9.142685),
+    new City ('Budapest', 47.497913, 19.040236),
+    new City ('New Delhi', 28.644800, 77.216721),
+    new City ('Cairo', 30.033333, 31.233334),
+    new City ('Paris', 48.864716, 2.349014),
+    new City ('London', 51.509865, -0.118092),
+    new City ('Athens', 37.983810, 23.727539),
+    new City ('Vienna', 48.210033, 16.363449),
+    new City ('Chicago', 41.881832, -87.623177),
+    new City ('Sydney', -33.865143, 151.209900),
+    new City ('Kiev', 50.450001, 30.523333),
+    new City ('Ankara', 39.925533, 32.866287),
+    new City ('Hamburg', 53.551086, 9.993682),
+    new City ('Florence', 43.769562, 11.255814),
+    new City ('Atlantis', 31.415538, -24.459229)
+    ]
+
+const activities = ['Santa is not leaving a single crumb back.',
+    'Santa is checking if the milk is spoiled.',
+    'Santa is dancing to the beat.',
+    'Santa is trying hard to fit the presents into the socks.',
+    'Santa is stuck in a chimney.',
+    'Santa is checking out the naughty list.',
+    'Santa sees that little Timmy is not on the list.',
+    'Santa is completing another Python tutorial while flying.',
+    'Santa is around the world, around the world.',
+    'Santa is full of cookies and milk.',
+    'Santa is growing an appreciation for K-Pop.',
+    'Santa is off to collect that Christmas EDM mixtape.',
+    'Santa is transporting an UBER passenger.']
 
 document.getElementById('searchButton').addEventListener("click", santaLocator);
+
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 function santaLocator() {
     const randomCityIndex = Math.floor(Math.random() * cities.length);
@@ -34,12 +87,12 @@ function santaLocator() {
     const randomActivityIndex = Math.floor(Math.random() * activities.length);
     const randomActivity = activities[randomActivityIndex]
 
-    document.getElementById('locationResult').innerText = `Santa is in ${randomCity} and ${randomActivity}`
+    document.getElementById('locationResult').innerText = `Found him in ${randomCity.name}!`
+    L.marker([randomCity.latitude, randomCity.longitude]).addTo(map);
+    map.setView([randomCity.latitude, randomCity.longitude], 13);
+    const popup = L.popup()
+        .setLatLng([randomCity.latitude, randomCity.longitude])
+        .setContent(`${randomActivity}`)
+        .openOn(map)
 }
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
