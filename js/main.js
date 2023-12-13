fec9bb5df3db2f04ed05d27e04e0150ecbf97261
sujade <dnmzsu@gmail.com>
2023-12-13 10:28:23 +0100

    Added holiday wish generator.

--- js/main.js
+++ js/main.js
@@ -112,3 +112,21 @@ function santaLocator() {
         .openOn(map)
 }
 
+const niceThingsToSay = [
+    'Warmest wishes!',
+    'Wishing you a wonderful holiday season!',
+    'Happy holidays!',
+    'Seasons greetings!',
+    'Warm and cozy holiday wishes!',
+    'Have a jolly holiday',
+    'Cheers to warm holiday memories!',
+    'Merry Christmas!'
+]
+
+document.getElementById('gift').addEventListener("click", holidayMessage);
+
+function holidayMessage(){
+    const randomWishIndex = Math.floor(Math.random() * niceThingsToSay.length);
+    const randomWish = niceThingsToSay[randomWishIndex]
+    document.getElementById('message').innerText = `${randomWish}`
+}
